64363be9258d0a012e5202cdc3d8873c7351c303
blubMe <fahmiirsyad10@gmail.com>
2018-10-02 00:34:04 +0700

    02 variables

---
+++ 02-variable/variable.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+//variable is some code who can hold , reference or point data.
+// variable need to be declared before can be used.
+// var { name } = { data }
+
+var str = "Hello, bubblegem & "
+var name = "Fahmi Irsyad Khairi"
+print(str + name)
